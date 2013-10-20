d717cb635f60a987badf3211d5674d582c91449f
Anton Ekblad <anton@ekblad.cc>
2013-10-21 00:07:17 +0200

    Make name and reference lookups case insensitive.

--- hs/Microscope/Query.hs
+++ hs/Microscope/Query.hs
@@ -1,15 +1,17 @@
 -- | Perform queries over a Microscope world.
 module Microscope.Query where
 import Control.Monad
+import Data.Char (toLower)
 import Microscope.Types
 
 -- | A list of all entities with the given tag.
 tagged :: World -> EntityTag -> [Entity]
-tagged w t = 
+tagged w t =
     go (worldEntities w)
   where
+    t' = lowerTag t
     go (e:es)
-      | t `elem` entityTags e =
+      | t' `elem` map lowerTag (entityTags e) =
         e : go es ++ allSubentities e
       | otherwise = 
         go es ++ go (entitySubentities e)
@@ -20,8 +22,20 @@ tagged w t =
 
 -- | The entity with the given name, if one exists.
 named :: World -> EntityName -> Maybe Entity
-named w n = 
+named w n =
     msum [named' e | e <- worldEntities w]
   where
-    named' e | entityName e == n = return e
-             | otherwise         = msum [named' e' | e' <- entitySubentities e]
+    n' = lowerName n
+    named' e
+      | lowerName (entityName e) == n' =
+        return e
+      | otherwise =
+        msum [named' e' | e' <- entitySubentities e]
+
+-- | Make an entity name lowercase.
+lowerName :: EntityName -> EntityName
+lowerName (EntityName n) = EntityName $ map toLower n
+
+-- | Make an entity tag lowercase.
+lowerTag :: EntityTag -> EntityTag
+lowerTag (EntityTag n) = EntityTag $ map toLower n
